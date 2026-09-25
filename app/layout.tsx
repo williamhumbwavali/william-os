8b1c9eed0dc5c00337c4d559b16b4b82443cf1e8
William <williamhumbwavali@gmail.com>
2026-09-25 16:53:39 +0100

    fix: remove bg-base from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
       lang="pt-AO"
       className={`${display.variable} ${body.variable} ${mono.variable}`}
     >
-      <body className="bg-base font-body text-black antialiased">
+      <body className="bg-white font-body text-black antialiased">
         {children}
       </body>
     </html>
